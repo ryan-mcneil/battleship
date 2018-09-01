291eec8913bf6f9cbd556f478a52d73b6064bff9
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-09-01 13:01:38 -0600

    Add build_row and build_row

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,26 @@
 class Board
+  attr_reader :grid
 
   def initialize()
-    @grid {}
+    @grid = Hash.new()
   end
+
+  def build_board
+    row_syms = ["A", "B", "C", "D"]
+    row_syms.each do |row_sym|
+      @grid[row_sym] = build_row
+    end
+  end
+
+
+  def build_row ()
+    row = Hash.new()
+    4.times do |col|
+      row[(col+1).to_s] = 0
+    end
+    return row
+  end
+
 
 
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -9,4 +9,22 @@ class BoardTest < Minitest::Test
     board = Board.new()
     assert_instance_of Board, board
   end
+
+  def test_it_can_build_board
+    board = Board.new()
+    expected = {"A" => {"1" => 0, "2" => 0, "3" => 0, "4" => 0 },
+                "B" => {"1" => 0, "2" => 0, "3" => 0, "4" => 0 },
+                "C" => {"1" => 0, "2" => 0, "3" => 0, "4" => 0 },
+                "D" => {"1" => 0, "2" => 0, "3" => 0, "4" => 0 }
+                }
+
+    board.build_board
+    assert_equal expected, board.grid
+  end
+
+  def test_it_can_build_a_row
+    board = Board.new()
+    expected = {"1" => 0, "2" => 0, "3" => 0, "4" => 0 }
+    assert_equal expected, board.build_row
+  end
 end
